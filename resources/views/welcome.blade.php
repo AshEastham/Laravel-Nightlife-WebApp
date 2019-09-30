4d43fa4473c90c801a5c27bf7c9ab8e48a6777f6
Ashley Malcolm Michael Eastham <a.eastham2354@student.leedsbeckett.ac.uk>
2019-09-30 10:27:44 +0000

    Updates welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ash's Site
                 </div>
 
                 <div class="links">
